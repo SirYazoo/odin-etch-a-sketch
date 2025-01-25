92a709cbe8aafb72cfa83a7a22162791e2597c31
SirYazoo <30350331+SirYazoo@users.noreply.github.com>
2025-01-25 18:51:53 +0700

    Add logic to change the grid size

--- script.js
+++ script.js
@@ -9,15 +9,33 @@ container.style.margin = "0";
 container.style.padding = "0";
 
 function createGrid(size) {
+    container.innerHTML = "";
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.style.width = `${100 / size}%`;
         square.style.height = `${100 / size}%`;
         square.style.boxSizing = "border-box";
         square.style.border = "1px solid black";
+        square.style.flexShrink = "0";
         square.addEventListener("mouseover", (event => {
             event.target.style.backgroundColor = "green";
         }));
         container.appendChild(square);
     }
 }
+
+const button = document.createElement("button");
+button.textContent = "Enter size"
+body.insertBefore(button, container);
+button.addEventListener("click", () => checkInput());
+
+function checkInput() {
+    const userInput = parseInt(prompt("Enter the grid size (Max. 100):"), 10);
+    if(userInput > 0 && userInput <= 100) {
+        createGrid(userInput);
+    } else {
+        alert("Needs to be more than 0 and less than 100");
+    }
+}
+
+createGrid(16);
